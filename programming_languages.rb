d32908e5133da0374d4333d96d6fcb3736d9c21f
han xu <hanxu27@gmail.com>
2019-04-14 02:48:07 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,9 +5,9 @@ def reformat_languages(languages)
   key = {}
   
   languages.each do |style, data|
-
     data.each do |language, type|
 binding.pry
+      key[language] = {:type: type, :style: style}
     end
   end
 end
